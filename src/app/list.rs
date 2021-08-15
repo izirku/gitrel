7ae295be0968469a2e7568d80f822b77b3556f88
izirku <izirku@users.noreply.github.com>
2021-08-15 18:41:59 -0500

    --wip-- [skip ci]

--- src/app/list.rs
+++ src/app/list.rs
@@ -1,4 +1,5 @@
 use crate::business::data::conf::model::PackageReqMap;
+use crate::foundation::util::svec2_col_maj_max_lens_unchecked;
 use anyhow::{Context, Result};
 use std::fs;
 use std::path::Path;
@@ -16,44 +17,31 @@ pub fn list_requested(file: &Path) -> Result<()> {
     for (name, pkg_spec) in toml.into_iter() {
         let ver = format!("@ {}", pkg_spec.get_version());
         let repo = format!("[https://github.com/{}]", pkg_spec.get_repo(&name));
-        cols.push((name, ver, repo));
+        cols.push(vec![name, ver, repo]);
     }
 
-    let lengths: Vec<_> = cols
-        .iter()
-        .map(|(n, v, r)| (n.len(), v.len(), r.len()))
-        .collect();
-
-    let max_len_name = lengths
-        .iter()
-        .max_by_key(|tup| tup.0)
-        .map(|tup| tup.0)
-        .unwrap();
-
-    let max_len_ver = lengths
-        .iter()
-        .max_by_key(|tup| tup.1)
-        .map(|tup| tup.1)
-        .unwrap();
+    let max_lens = svec2_col_maj_max_lens_unchecked(&cols);
 
     println!(
         "{:<w_name$} {:<w_ver$} {}\n",
         "BIN",
         "VER",
         "REPO",
-        w_name = max_len_name,
-        w_ver = max_len_ver
+        w_name = max_lens[0],
+        w_ver = max_lens[1],
     );
 
-    for (name, ver, repo) in &cols {
-        println!(
-            "{:<w_name$} {:<w_ver$} {}",
-            name,
-            ver,
-            repo,
-            w_name = max_len_name,
-            w_ver = max_len_ver
-        );
+    for row in &cols {
+        if let [name, ver, repo] = &row[..] {
+            println!(
+                "{:<w_name$} {:<w_ver$} {}",
+                name,
+                ver,
+                repo,
+                w_name = max_lens[0],
+                w_ver = max_lens[1],
+            );
+        }
     }
     Ok(())
 }
--- src/foundation/mod.rs
+++ src/foundation/mod.rs
@@ -1,2 +1,3 @@
 pub mod consts;
 pub mod file;
+pub mod util;
