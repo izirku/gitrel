6075af285a28d799b99d6d26a9bf9d68e4900519
izirku <izirku@users.noreply.github.com>
2021-09-21 13:28:05 -0500

    feat: add rx to match OS and platform

--- src/business/rx.rs
+++ src/business/rx.rs
@@ -4,13 +4,118 @@ use regex::Regex;
 lazy_static! {
     // source: https://regex101.com/r/Ly7O1x/1070
     pub static ref SEMVER: Regex = Regex::new(r"(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?").expect("error parsing regex");
+}
 
-// regex crate doesnt support branch reset...
-//     pub static ref REPO_SPEC: Regex = Regex::new(r"^(?:(\w[\w-]*)|(?:(\w[\w-]*)@(.*))|(?:(\w[\w-]*)/(\w[\w-]*))|(?:(\w[\w-]*)/(\w[\w-]*)@(.*)))$").expect("error parsing regex");
-//     pub static ref REPO_SPEC: RegexSet = RegexSet::new(&[
-//         r"^(?P<repo>\w[\w-]*)$",
-//         r"^(?P<repo>\w[\w-]*)@(?P<tag>.*)$",
-//         r"^(?P<user>\w[\w-]*)/(?P<repo>\w[\w-]*)$",
-//         r"^(?P<user>\w[\w-]*)/(?P<repo>\w[\w-]*)@(?P<tag>.*)$",
-//         ]).expect("error parsing regex");
+// =================================================================================
+// MATCH_OS
+
+// These values are those supported by Rust (based on the platforms
+// crate) and Go (based on
+// https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63).
+
+#[cfg(target_os = "linux")]
+lazy_static! {
+    pub static ref MATCH_OS: Regex =
+        Regex::new(r"(?i:(?:\b|_)linux(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_os = "freebsd")]
+lazy_static! {
+    pub static ref MATCH_OS: Regex =
+        Regex::new(r"(?i:(?:\b|_)freebsd(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_os = "openbsd")]
+lazy_static! {
+    pub static ref MATCH_OS: Regex =
+        Regex::new(r"(?i:(?:\b|_)openbsd(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_os = "macos")]
+lazy_static! {
+    pub static ref MATCH_OS: Regex =
+        Regex::new(r"(?i:(?:\b|_)(?:darwin|macos)(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_os = "windows")]
+lazy_static! {
+    pub static ref MATCH_OS: Regex =
+        Regex::new(r"(?i:(?:\b|_)windows(?:\b|_))").expect("error parsing regex");
+}
+
+// =================================================================================
+// MATCH_ARCH
+
+#[cfg(target_arch = "aarch64")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)aarch64(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "arm")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)arm(?:v[0-9]+|64)?(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "mips")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)mips(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "mips64")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)mips64(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "powerpc")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)(?:powerpc32(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "powerpc64")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)(?:powerpc64|ppc64(?:le|be)?)?)(?:\b|_))")
+            .expect("error parsing regex");
+}
+
+#[cfg(target_arch = "riscv")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)riscv(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "s390x")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)s390x(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "sparc")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)sparc(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "sparc64")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)sparc(?:64)?(?:\b|_))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "x86")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)(?:x86|386)(?:\b|_(?!64)))").expect("error parsing regex");
+}
+
+#[cfg(target_arch = "x86_64")]
+lazy_static! {
+    pub static ref MATCH_ARCH: Regex =
+        Regex::new(r"(?i:(?:\b|_)(?:x86_64|amd64)(?:\b|_))").expect("error parsing regex");
+        // Regex::new(r"(?i:(?:\b|_)(?:x86|386|x86_64|amd64)(?:\b|_))").expect("error parsing regex");
 }
